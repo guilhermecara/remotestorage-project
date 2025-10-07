c19c013c49239596dacac43b4cf79dd6fb6f45c1
guilhermecara <guilhermecaraujoo1@gmail.com>
2025-10-07 00:03:36 -0400

    Added actual paths to the images on the API.

--- remotestorage-db/migrations/002-add-initial-entries.sql
+++ remotestorage-db/migrations/002-add-initial-entries.sql
@@ -1,6 +1,6 @@
 INSERT INTO images (name, url, publication_year)
 VALUES
-  ('Sample Image 1', 'imagem 1 url', '2022-01-01'),
-  ('Sample Image 2', 'imagem 2 url', '2023-02-15'),
-  ('Sample Image 3', 'imagem 3 url', '2024-03-20')
+  ('Sample Image 1', '/1', '2022-01-01'),
+  ('Sample Image 2', '/2', '2023-02-15'),
+  ('Sample Image 3', '/3', '2024-03-20')
 ON CONFLICT DO NOTHING;
